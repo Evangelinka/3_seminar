50a0213f742fad8dd6778ff89bd977cda8c05919
«Kristina» <kristinabol1996@gmail.com>
2022-08-16 23:48:44 +0800

    3 задача

---
+++ Task003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние 
+// между ними в 2D пространтсве
+
+double Distance(double x1, double y1, double x2, double y2)
+{
+    double dist = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+    return dist;
+}
+
+Console.WriteLine(Distance(3,6,2,1));
+Console.WriteLine(Distance(7,-5,1,-1));
