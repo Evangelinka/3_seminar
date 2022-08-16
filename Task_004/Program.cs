08557de8ad92064a04485a555ed78c5854ecee49
«Kristina» <kristinabol1996@gmail.com>
2022-08-16 23:49:35 +0800

    4 задача

---
+++ Task_004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N
+
+void Squares(int N)
+{
+    int i = 1;
+    while(i < N)
+    {
+        Console.Write($"{Math.Pow(i,2)}, ");
+        i++;
+    }
+    Console.Write($"{Math.Pow(i,2)}. ");
+}
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Squares(number);
